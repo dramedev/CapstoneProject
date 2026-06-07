e4223dff61a38e43a4cf6546e2973d433324bc1f
dramedev <techwithdaouda@gmail.com>
2026-06-07 15:37:52 +0300

    Changes on models

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': 'Dd-7025650505',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+    name = models.CharField( max_length=50)
+    last_name =models.CharField( max_length=50)
+    no_of_guest = models.IntegerField()
+    date =models.DateTimeField()
+    
+    def __str__(self):
+        return self.name ,self.last_name
+    
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    inventory = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
