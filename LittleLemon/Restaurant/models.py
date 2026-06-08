9c02ff74caf71a49e93bebed80ae63a331f196ca
dramedev <techwithdaouda@gmail.com>
2026-06-08 15:29:11 +0300

    testing api

--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -11,10 +11,10 @@ class Booking(models.Model):
     
 
 class Menu(models.Model):
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
     inventory = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return self.name
+        return f'{self.title} : {str(self.price)}'
--- LittleLemon/Restaurant/tests.py
+++ LittleLemon/Restaurant/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from Restaurant.models import Menu
 
-# Create your tests here.
+
+class MenuTest(TestCase):
+
+    def test_menu_name(self):
+        item = Menu.objects.create(
+            title="IceCream",
+            price=80,
+            description="Vanilla ice cream",
+            inventory=100
+        )
+
+        self.assertEqual(str(item), "IceCream")
